d6e80061d395cb59679c9db8bea1bb5a316cdc60
tetianadac <datsenkotetiana027@gmail.com>
2022-08-31 15:57:35 +0300

    modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,38 +1,35 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-      body: document.querySelector('[data-page]'),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-     refs.body.classList.toggle('no-scroll');
-  }
-})();
-
-
 // (() => {
 //   const refs = {
-//     openModalBtns: document.querySelectorAll('[data-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//       body: document.querySelector('[data-page]'),
 //   };
-// ​
-//   refs.openModalBtns.forEach(btn => btn.addEventListener('click', toggleModal));
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-// ​
-//   function toggleModal() {
-//     refs.modal.classList.toggle('backdrop--hidden');
-//          refs.body.classList.toggle('no-scroll');
 
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//      refs.body.classList.toggle('no-scroll');
 //   }
 // })();
-// Свернуть
+
+
+(() => {
+  const refs = {
+    openModalBtns: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+  refs.openModalBtns.forEach(btn => btn.addEventListener('click', toggleModal));
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop--hidden');
+   refs.body.classList.toggle('no-scroll');
+  }
+})();
+Свернуть
 
 
 
