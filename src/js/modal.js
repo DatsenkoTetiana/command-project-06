e3f606b917409da35c760ea5ea90599cc7f97a98
tetianadac <datsenkotetiana027@gmail.com>
2022-08-31 18:31:22 +0300

    modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,6 +4,8 @@
     openModalBtns: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+          body: document.querySelector('[data-page]'),
+
   };
   refs.openModalBtns.forEach(btn => btn.addEventListener('click', toggleModal));
   refs.closeModalBtn.addEventListener('click', toggleModal);
